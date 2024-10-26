b9f2ef7bcde491cae59a05d1aa518eddc142c403
Javier Pestana <javier.pestana85@gmail.com>
2024-10-26 14:35:02 +0100

    button submit css

--- formkit.config.js
+++ formkit.config.js
@@ -8,21 +8,14 @@ const config = {
                 wrapper: "space-y-2 mb-3",
                 input: "w-full p-3 border border-gray-300 rounded text-gray-700 placeholder-gray-400"
             },
-            text: {
-
-            },
             file: {
                 noFiles: "block my-2",
                 fileItem: "hidden"
 
             },
-            select: {
-
-
-            },
-            number: {
-
-            },
+            submit: {
+                input: "$reset bg-green-400 hover:bg-green-500 w-full p-2 font-bold uppercase disabled:opacity-50"
+            }
         })
     }
 }
